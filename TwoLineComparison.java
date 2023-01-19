d86d36cf7730cde80a6b5c0e64d2a775bda18aa7
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-01-19 18:26:10 +0530

    day3LinecomparisonProblem

---
+++ TwoLineComparison.java
@@ -0,0 +1,10 @@
+package com.LineComparision;
+
+public class TwoLineComparison {
+    public static void main(String[] args){
+
+        System.out.println("Hey welcome in line comparison");
+
+
+    }
+}
