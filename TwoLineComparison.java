bb722b00911cf6c0a3be0b4a9975dc8de7b7af5f
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-01-19 18:56:05 +0530

    lines are equal or not

--- TwoLineComparison.java
+++ TwoLineComparison.java
@@ -13,11 +13,14 @@ public class TwoLineComparison {
         int x22 = 5;
         int y22 = 6;
         // formula for calculating length of line
-        float len1 = (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y2 - y1) * (y2 - y1)); //2*2 =4 2^2 =4
+        float len1 = (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y2 - y1) * (y2 - y1));
         float len2 = (float) Math.sqrt((x12 - x22) * (x12 - x22) + (y22 - y12) * (y22 - y12));
         System.out.println(len1);
         System.out.println(len2);
+        //check the length of equal or not
+        if (len1 == len2) {
+            System.out.println("Both lines are Equal");
+        }
 
-
-    }
+        }
 }
